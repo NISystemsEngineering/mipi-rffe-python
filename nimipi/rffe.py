f21ec78c0393f58d0a72b52354665bc5ad4ce2fa
Sean Moore <sean.moore@ni.com>
2020-02-17 13:23:39 -0600

    add support for extended register reads and writes

--- nimipi/rffe.py
+++ nimipi/rffe.py
@@ -106,6 +106,58 @@ def __convert_string_to_numeric_array(string):
     return [int(char) for char in string]
 
 
+def __calc_byte_count(command, write_data):
+    """ Calculates byte count based on data to be written. """
+    byte_count = len(write_data)
+    if command == __Command.REG_WRITE_EXT_LONG:
+        if byte_count < 1:
+            byte_count = 1
+        elif byte_count > 8:
+            byte_count = 8
+    return byte_count
+
+
+def __calc_addition_for_parity(command, byte_count):
+    """ Adds appropriate sizing and ones to the string for the parity calculation. """
+    byte_count = byte_count - 1 # BC is 0 indexed 0b0000 means 1 byte Decrement BC by 1 
+    if command == __Command.REG_WRITE_EXT: # 0000 + BC[3:0] Even parity on first 4 bits, so no addition required for proper parity calculation
+        result_string = "{:04b}".format(byte_count)
+        byte_count_string = result_string
+    elif command == __Command.REG_READ_EXT or command == __Command.REG_READ_EXT_HR: # 0010 + BC[3:0] Odd parity on first 4 bits, so concatenate a 1 to the string for correct parity calculation
+        byte_count_string = "{:04b}".format(byte_count)
+        result_string = '1' + byte_count_string
+    elif command == __Command.REG_WRITE_EXT_LONG: # 00110 + BC[2:0] Even parity on first 5 bits, so no addition required for proper parity calculation
+        result_string = "{:03b}".format(byte_count)
+        byte_count_string = result_string
+    elif command == __Command.REG_READ_EXT_LONG or command == __Command.REG_READ_EXT_LONG_HR: # 00111 + BC[2:0] Odd parity on first 5 bits, so concatenate a 1 to the string for correct parity calculation
+        byte_count_string = "{:03b}".format(byte_count)
+        result_string = '1' + byte_count_string
+    else:
+        RffeException(5000, "The specified command " + command.name + " is not supported.")
+    return (result_string, byte_count_string)
+
+
+def __calc_loop_count(command):
+    """ This is a simple way to control loop count for the register address.
+        If the command is a extended long command, it will use a 16-bit address that needs two loops.
+        Else, the command only needs one loop for a 8-bit address """
+    if command == __Command.REG_WRITE_EXT_LONG or command == __Command.REG_READ_EXT_LONG or command == __Command.REG_READ_EXT_LONG_HR:
+        return 2
+    return 1
+    
+
+def __value_to_format(value):
+    value_hex = "{:02X}".format(value)
+    value_hex_len = len(value_hex)
+    value_bin_len = 4 * value_hex_len
+    return "0{:d}b".format(value_bin_len)
+
+
+def __data_to_string(write_data):
+    """ Converts data array to string."""
+    return "".join(["{:08b}".format(data) for data in write_data])
+
+
 def __create_source_waveform_data(command, register_data):
     """ Creates data to write into source waveform. """
     assert isinstance(register_data, RegisterData)
@@ -141,6 +193,44 @@ def __create_source_waveform_data(command, register_data):
         parity_bit = __parity_calc(slave_address_bit_string + command_bits + register_address_bit_string)
         data_frame = slave_address_bit_string + register_address_bit_string + parity_bit
         return (1, __convert_string_to_numeric_array(data_frame))
+    if command == __Command.REG_WRITE_EXT or command == __Command.REG_WRITE_EXT_LONG:
+        # For extended Register Writes, build 4 bit SA, 4/3 bit BC, parity, 8/16 bit Addr with partity, and 16/8 bytes of data with parity
+        byte_count = __calc_byte_count(command, register_data.write_data)
+        result_string, byte_count_string = __calc_addition_for_parity(command, byte_count)
+        slave_address_bit_string = "{:04b}".format(register_data.slave_address)
+        parity_bit = __parity_calc(slave_address_bit_string + result_string)
+        command_frame = slave_address_bit_string + byte_count_string + parity_bit
+        format_string = __value_to_format(register_data.register_address)
+        register_address_bit_string = "{:{:s}}".format(register_data.register_address, format_string)
+        address_frame = ""
+        for i in range(__calc_loop_count(command)):
+            register_address_byte_string = register_address_bit_string[i*8:i*8 + 8]
+            parity_bit = __parity_calc(register_address_byte_string)
+            address_frame = address_frame + register_address_byte_string + parity_bit
+        write_data_bit_string = __data_to_string(register_data.write_data)
+        data_frame = ""
+        for i in range(byte_count):
+            write_data_byte_string = write_data_bit_string[i*8:i*8 + 8]
+            parity_bit = __parity_calc(write_data_byte_string)
+            data_frame = data_frame + write_data_byte_string + parity_bit
+        frame = command_frame + address_frame + data_frame
+        padding = '0' * (163 - len(frame))
+        return (byte_count, __convert_string_to_numeric_array(frame + padding))
+    if command == __Command.REG_READ_EXT or command == __Command.REG_READ_EXT_HR or command == __Command.REG_READ_EXT_LONG or command == __Command.REG_READ_EXT_LONG_HR:
+        # For extended Register Reads, build 4 bit SA, 4/3 bit BC, and 8/16 bit Addr. Add two bus park cycles based on data frame format.
+        result_string, byte_count_string = __calc_addition_for_parity(command, register_data.byte_count)
+        slave_address_bit_string = "{:04b}".format(register_data.slave_address)
+        parity_bit = __parity_calc(slave_address_bit_string + result_string)
+        command_frame = slave_address_bit_string + byte_count_string + parity_bit
+        format_string = __value_to_format(register_data.register_address)
+        register_address_bit_string = "{:{:s}}".format(register_data.register_address, format_string)
+        address_frame = ""
+        for i in range(__calc_loop_count(command)):
+            register_address_byte_string = register_address_bit_string[i*8:i*8 + 8]
+            parity_bit = __parity_calc(register_address_byte_string)
+            address_frame = address_frame + register_address_byte_string + parity_bit
+        frame = command_frame + address_frame # For ext reads, only capturing data + parity
+        return (register_data.byte_count, __convert_string_to_numeric_array(frame))
     raise RffeException(5000, "The specified command " + command.name + " is not supported.")
 
 
@@ -154,9 +244,12 @@ def __burst_command(session, pin_name, command, register_data):
     session.write_source_waveform_broadcast(source_waveform_name, source_waveform_data)
     session.write_sequencer_register("reg0", byte_count)
     session.burst_pattern("", source_waveform_name, True, True, 10)
-    if command == __Command.REG_0_WRITE or command == __Command.REG_WRITE:
+    if command == __Command.REG_0_WRITE or command == __Command.REG_WRITE \
+        or command == __Command.REG_WRITE_EXT or command == __Command.REG_WRITE_EXT_LONG:
         return
-    if command == __Command.REG_READ:
+    if command == __Command.REG_READ or command == __Command.REG_READ_HR \
+        or command == __Command.REG_READ_EXT or command == __Command.REG_READ_EXT_HR \
+        or command == __Command.REG_READ_EXT_LONG or command == __Command.REG_READ_EXT_LONG_HR:
         capture_waveform = session.fetch_capture_waveform("", source_waveform_name, register_data.byte_count, 1)
         return list(capture_waveform[0])
     raise RffeException(5000, "The specified command " + command.name + " is not supported.")
@@ -194,17 +287,32 @@ def reg0_write(session, pin_name, register_data):
 
 
 def reg_write(session, pin_name, register_data):
-    """ Bursts a RegWrite command. This function returns None. """
+    """ Bursts a standard register write command. This function returns None. """
     return __burst_command(session, pin_name, __Command.REG_WRITE, register_data)
 
 
 def reg_read(session, pin_name, register_data):
-    """ Bursts a RegRead command. This function returns one channel of read data. """
+    """ Bursts a standard register read command. This function returns one channel of read data. """
     return __burst_command(session, pin_name, __Command.REG_READ, register_data)
 
+def extended_reg_write(session, pin_name, register_data):
+    """ Bursts an extended register write command. """
+    return __burst_command(session, pin_name, __Command.REG_WRITE_EXT, register_data)
+
+def extended_reg_read(session, pin_name, register_data):
+    """ Bursts an extended register read command. """
+    return __burst_command(session, pin_name, __Command.REG_READ_EXT, register_data)
+
+def extended_reg_write_long(session, pin_name, register_data):
+    """ Bursts an extended register write long command. """
+    return __burst_command(session, pin_name, __Command.REG_WRITE_EXT_LONG, register_data)
+
+def extended_reg_read_long(session, pin_name, register_data):
+    """ Bursts an extended register read long command. """
+    return __burst_command(session, pin_name, __Command.REG_READ_EXT_LONG, register_data)
 
 def multi_command(session, pin_name, multi_command_write, multi_command_read):
     """ Uses looping to execute multiple register writes and register reads. """
     for write_command in multi_command_write:
-        reg_write(session, pin_name, write_command)
-    return [reg_read(session, pin_name, read_command) for read_command in multi_command_read]
+        extended_reg_write(session, pin_name, write_command)
+    return [extended_reg_read(session, pin_name, read_command) for read_command in multi_command_read]
